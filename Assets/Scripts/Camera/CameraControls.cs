7b0b21ed4b18ce30edf85ae9b1005cdb2aa9f87c
WalkerWilliams <wwilli47@uncc.edu>
2020-01-29 20:59:27 -0500

    Fixing some gitignore stuff

---
+++ Assets/Scripts/Camera/CameraControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+    /*
+     * Plans:
+     * - Limit directions that camera can go based on parameters (serialized fields)
+     * - Disabliing or enabling the camera controls/user input.
+     * - Range of fields/angles based on input parameters (serialized fields)
+     * - 
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
