8318a7743d60c2ec2caf91cd976fb5903ff6e732
WalkerWilliams <wwilli47@uncc.edu>
2020-02-06 20:47:23 -0500

    Messing with Ball Physics
    Added SerializeFields for ball velocity.

--- Assets/Scripts/Pong/PongBallMovement.cs
+++ Assets/Scripts/Pong/PongBallMovement.cs
@@ -6,12 +6,27 @@ public class PongBallMovement : MonoBehaviour
 {
 
     [SerializeField] Rigidbody ball;
-    [SerializeField] float speed;
+    [SerializeField] float Xspeed;
+    [SerializeField] float Yspeed;
+
+    [SerializeField] bool randomizeSpeed;
+    [SerializeField] int lower_random_speed_limit;
+    [SerializeField] int uppwer_random_speed_limit;
 
     // Start is called before the first frame update
     void Start()
     {
-        ball.velocity = Vector3.right * speed;
+        if (randomizeSpeed)
+        {
+            Xspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
+            Yspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
+
+            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
+        }
+        else
+        {
+            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
+        }
     }
 
     // Update is called once per frame
@@ -25,22 +40,22 @@ public class PongBallMovement : MonoBehaviour
         if (collision.gameObject.name == "Upper Bound") {
             //send down - y
             Debug.Log("Hitting the uppper bound, sending it down now.");
-            applyForceToBall(Vector3.down * 2 * speed);
+            applyForceToBall(Vector3.down * 2 * Yspeed);
         }
         else if (collision.gameObject.name == "Lower Bound")
         {
             //send up - y
-            applyForceToBall(Vector3.up * 2 * speed);
+            applyForceToBall(Vector3.up * 2 * Yspeed);
         }
         else if (collision.gameObject.name == "Player")
         {
             //send right - x
-            applyForceToBall(Vector3.right * 2 * speed);
+            applyForceToBall(Vector3.right * 2 * Xspeed);
         }
         else if (collision.gameObject.name == "Enemy")
         {
             //send left = x
-            applyForceToBall(Vector3.left * 2 * speed);
+            applyForceToBall(Vector3.left * 2 * Xspeed);
         }
     }
 
