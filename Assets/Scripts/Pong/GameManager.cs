6f1fb4fe481256c49181a5788b191837da548780
ovidiu mocanu <omocanu@uncc.edu>
2020-02-08 04:54:27 -0500

    Fixed ball movement and added way to keep track of score

---
+++ Assets/Scripts/Pong/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public int playerScore = 0;
+    public int enemyScore = 0;
+    [SerializeField] private GameObject pongBall;
+    [SerializeField] private PongBallMovement pongBallScript;
+    private bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        if (!pongBall.activeSelf & !started) {
+            StartCoroutine(SpawnBall());
+        }
+        
+    }
+
+    IEnumerator SpawnBall() {
+        started = true;
+        yield return new WaitForSecondsRealtime(5);
+        pongBall.SetActive(true);
+        started = false;
+        Debug.Log("turning on ball");
+        yield return new WaitForSecondsRealtime(5);
+        pongBallScript.BeginMovement();
+        
+    }
+    public void updatePlayerScore() {
+        playerScore += 1;
+        Debug.Log(playerScore);
+    }
+
+    public void updateEnemyScore() {
+        enemyScore += 1;
+        Debug.Log(enemyScore);
+    }
+
+
+}
--- Assets/Scripts/Pong/PongBallMovement.cs
+++ Assets/Scripts/Pong/PongBallMovement.cs
@@ -6,27 +6,20 @@ public class PongBallMovement : MonoBehaviour
 {
 
     [SerializeField] Rigidbody ball;
+    [SerializeField] GameObject pongBall;
     [SerializeField] float Xspeed;
     [SerializeField] float Yspeed;
+
+    [SerializeField] GameManager gameManager;
+
 
     [SerializeField] bool randomizeSpeed;
     [SerializeField] int lower_random_speed_limit;
     [SerializeField] int uppwer_random_speed_limit;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        if (randomizeSpeed)
-        {
-            Xspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
-            Yspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
-
-            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
-        }
-        else
-        {
-            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
-        }
+    void Start(){
+        BeginMovement();
     }
 
     // Update is called once per frame
@@ -35,29 +28,112 @@ public class PongBallMovement : MonoBehaviour
         
     }
 
+    /*
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Upper Bound") {
+        
+        if (collision.gameObject.name == "Upper Bound" || collision.gameObject.name == "Lower Bound") {
             //send down - y
-            Debug.Log("Hitting the uppper bound, sending it down now.");
-            applyForceToBall(Vector3.down * 2 * Yspeed);
+            //Debug.Log("Hitting the uppper bound, sending it down now.");
+            //applyForceToBall(Vector3.down * 2 * Yspeed);
+
+            //invert y velocity
+            Debug.Log("hit upper or lower boundry");
+            ball.velocity = new Vector3(Xspeed, -Yspeed, 0f);
+            Yspeed = -Yspeed;
         }
-        else if (collision.gameObject.name == "Lower Bound")
-        {
-            //send up - y
-            applyForceToBall(Vector3.up * 2 * Yspeed);
-        }
-        else if (collision.gameObject.name == "Player")
+        
+        else if (collision.gameObject.name == "Player" || collision.gameObject.name == "Enemy")
         {
             //send right - x
-            applyForceToBall(Vector3.right * 2 * Xspeed);
+            //applyForceToBall(Vector3.right * 2 * Xspeed);
+            Debug.Log("hit a paddle");
+            ball.velocity = new Vector3(-Xspeed, Yspeed, 0f);
+            Xspeed = -Xspeed;
         }
-        else if (collision.gameObject.name == "Enemy")
-        {
-            //send left = x
-            applyForceToBall(Vector3.left * 2 * Xspeed);
+
+        //score
+        else if (collision.gameObject.name == "Player Score Zone") {
+            pongBall.SetActive(false);
+            pongBall.transform.SetPositionAndRotation(Vector3.zero, pongBall.transform.rotation);            
+            gameManager.updateEnemyScore();
+
+            //insert explosion here
+
         }
+        else if (collision.gameObject.name == "Enemy Score Zone") {
+            pongBall.SetActive(false);
+            pongBall.transform.SetPositionAndRotation(Vector3.zero, pongBall.transform.rotation);            
+            gameManager.updatePlayerScore();
+
+            //insert explosion here
+
+        }*/
+
+
+    void OnTriggerEnter(Collider collision) {
+
+        if (collision.gameObject.name == "Upper Bound" || collision.gameObject.name == "Lower Bound") {
+            //send down - y
+            //Debug.Log("Hitting the uppper bound, sending it down now.");
+            //applyForceToBall(Vector3.down * 2 * Yspeed);
+
+            //invert y velocity
+            Debug.Log("hit upper or lower boundry");
+            ball.velocity = new Vector3(Xspeed, -Yspeed, 0f);
+            Yspeed = -Yspeed;
+        }
+
+        else if (collision.gameObject.name == "Player" || collision.gameObject.name == "Enemy") {
+            //send right - x
+            //applyForceToBall(Vector3.right * 2 * Xspeed);
+            Debug.Log("hit a paddle");
+            ball.velocity = new Vector3(-Xspeed, Yspeed, 0f);
+            Xspeed = -Xspeed;
+        }
+
+        //score
+        else if (collision.gameObject.name == "Player Score Zone") {
+            pongBall.SetActive(false);
+            pongBall.transform.SetPositionAndRotation(Vector3.zero, pongBall.transform.rotation);
+            gameManager.updateEnemyScore();
+
+            //insert explosion here
+
+        }
+        else if (collision.gameObject.name == "Enemy Score Zone") {
+            pongBall.SetActive(false);
+            pongBall.transform.SetPositionAndRotation(Vector3.zero, pongBall.transform.rotation);
+            gameManager.updatePlayerScore();
+
+            //insert explosion here
+
+        }
+        
+
+
     }
+    //apply a range angle and start ball movement
+    public void BeginMovement() {
+        /*
+        if (randomizeSpeed) {
+            Xspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
+            Yspeed = Random.Range(lower_random_speed_limit, uppwer_random_speed_limit);
+
+            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
+        }
+        else {
+            ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
+        }*/
+        int randomAngle = (int)Random.Range(-50,50) + (int)Random.Range(0, 1) * 180;
+        Yspeed = 10 * Mathf.Sin(randomAngle);
+        Xspeed = 10 * Mathf.Cos(randomAngle);
+        ball.velocity = new Vector3(Xspeed, Yspeed, 0f);
+
+
+
+    }
+
 
     void applyForceToBall(Vector3 direction)
     {
