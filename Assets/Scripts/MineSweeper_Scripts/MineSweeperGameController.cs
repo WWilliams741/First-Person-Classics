5b44474fbc977a6615ca57dda0027cc0477e5578
ovidiu mocanu <omocanu@uncc.edu>
2020-03-17 20:41:48 -0400

    added tiles for minesweeper

---
+++ Assets/Scripts/MineSweeper_Scripts/MineSweeperGameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeperGameController : MonoBehaviour{
+    [SerializeField] MineSweeperTileController[] tileArray;
+    private int[][] bombArray;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            for (int i = 0; i < tileArray.Length; i ++) {
+                tileArray[i].revealTile(i);
+
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/MineSweeper_Scripts/MineSweeperTileController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class MineSweeperTileController : MonoBehaviour {
+    [SerializeField] Renderer tileMaterial;
+    [SerializeField] GameObject tile;
+    [SerializeField] Material darkerTile;
+    [SerializeField] TextMeshProUGUI mineCount;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void revealTile(int TileValue) {
+        Vector3 newPos = new Vector3(tile.transform.position.x, tile.transform.position.y - 0.2f, tile.transform.position.z);
+        tile.transform.position = Vector3.Lerp(tile.transform.position, newPos, 1.0f);
+        tileMaterial.material = darkerTile;
+        mineCount.text = TileValue.ToString();
+
+    }
+
+
+}
