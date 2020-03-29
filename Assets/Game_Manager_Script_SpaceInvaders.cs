a9ac09d15d01ecac14e57f727fb02846f9d50d08
WalkerWilliams <wwilli47@uncc.edu>
2020-03-29 19:58:18 -0400

    Added Scripts and API for moving rows of Invaders
    API is mostly done just need to finish implementing the actual game logic. Maybe add some more to the game API.

---
+++ Assets/Game_Manager_Script_SpaceInvaders.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager_Script_SpaceInvaders : MonoBehaviour
+{
+
+    [SerializeField] RowManager_Script[] rowManagers;
+
+    private Coroutine[] rows;
+
+    private float waitTime;
+
+    private int leftMost;
+    private int rightMost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = updateWaitTime();
+        rows = new Coroutine[rowManagers.Length];
+
+        moveRowsRight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getLeftMost()
+    {
+        int left = rowManagers[0].Invaders.Length;
+        for (int i = 0; i < rowManagers.Length; i++)
+        {
+            int leftMost = rowManagers[i].getLeftMost();
+            if (left > leftMost)
+            {
+                left = leftMost;
+            }
+        }
+
+        return left;
+    }
+
+    public int getRightMost()
+    {
+        int right = 0;
+        for (int i = 0; i < rowManagers.Length; i++)
+        {
+            int rightMost = rowManagers[i].getRightMost();
+            if (right < rightMost)
+            {
+                right = rightMost;
+            }
+        }
+
+        return right;
+    }
+
+    public void moveRowsLeft()
+    {
+        for (int i = 0; i < rows.Length; i++)
+        {
+            rows[i] = StartCoroutine(rowManagers[i].moveLeft(waitTime));
+        }
+    }
+
+    public void moveRowsRight()
+    {
+        for (int i = 0; i < rows.Length; i++)
+        {
+            rows[i] = StartCoroutine(rowManagers[i].moveRight(waitTime));
+        }
+    }
+
+    public void moveRowsDown()
+    {
+        for (int i = 0; i < rowManagers.Length; i++)
+        {
+            rowManagers[i].moveDown();
+        }
+    }
+
+    public int getTotalAlive()
+    {
+        int alive = 0;
+        for (int i = 0; i < rowManagers.Length; i++)
+        {
+            alive += rowManagers[i].getAlive();
+        }
+
+        return alive;
+    }
+
+    public float updateWaitTime()
+    {
+        int totalAlive = getTotalAlive();
+
+        return Mathf.Log(totalAlive, 2f) + 0.5f;
+    }
+}
---
+++ Assets/RowManager_Script.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowManager_Script : MonoBehaviour
+{
+
+    public GameObject[] Invaders;
+
+    private int alive;
+    public bool paused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alive = Invaders.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getLeftMost()
+    {
+        for (int i = 0; i < Invaders.Length; i++)
+        {
+            if(Invaders[i].activeSelf)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getRightMost()
+    {
+        for (int i = Invaders.Length - 1; i >= 0; i--)
+        {
+            if (Invaders[i].activeSelf)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getAlive()
+    {
+        return alive;
+    }
+
+    public void killInvader(int i)
+    {
+        Invaders[i].SetActive(false);
+        alive--;
+    }
+
+    public void resetInvaders()
+    {
+        for (int i = 0; i < Invaders.Length; i++)
+        {
+            Invaders[i].SetActive(true);
+        }
+        alive = Invaders.Length;
+    }
+
+    public IEnumerator moveLeft(float waitTime)
+    {
+        while(!paused)
+        {
+            yield return new WaitForSecondsRealtime(waitTime);
+            transform.position = new Vector3(transform.position.x - 2f, transform.position.y, transform.position.z);
+        }
+    }
+
+    public IEnumerator moveRight(float waitTime)
+    {
+        while (!paused)
+        {
+            yield return new WaitForSecondsRealtime(waitTime);
+            transform.position = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void moveDown()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2f);
+    }
+}
